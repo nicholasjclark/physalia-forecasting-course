6b85288e89718b76fdc39a3bdeeaf5fccc48b62b
Nicholas Clark <nicholas.j.clark1214@gmail.com>
2024-05-15 15:50:48 +1000

    update tut4

--- day4/Tutorial4_exercises.R
+++ day4/Tutorial4_exercises.R
@@ -1,4 +1,4 @@
-#### "Ecological forecasting in R" Tutorial 3 exercises ####
+#### "Ecological forecasting in R" Tutorial 4 exercises ####
 # Written by Nicholas Clark; https://github.com/nicholasjclark
 
 #### Load all data and modelling objects needed to complete exercises ####
@@ -35,23 +35,12 @@ library(gratia)
 library(ggplot2)
 library(marginaleffects)
 
-#### Manipulating data exercises ####
+#### Manipulating for modeling exercises ####
 # 1. Calculate the number of timepoints in the training data 
 #    that have non-missing observations for all eight time series. 
 
 
-#### Modelling tick abundance exercises ####
-# 1. Fit each of the tick models using mgcv and interrogate them 
-#    (hint, use `method = 'REML` for the best smoothing parameter selection results)
-?gam
-
-# 2. If you were selecting among the three mgcv models using approximate 
-#    hypothesis tests with a generalized likelihood ratio test 
-#    (using `anova.gam`), which model would you end up with? 
-#    (hint, use `test = 'Chisq'` to perform model comparisons)
-?anova.gam
-
-#### State-Space VAR model exercises ####
+#### Multivariate model exercises ####
 # 1. Plot conditional effects of month and temperature for each 
 #    algal group. Hint, see the documentation in 
 #    ?marginaleffects::plot_predictions and use the following template 
@@ -62,30 +51,18 @@ library(marginaleffects)
 #    This will feed a particular grid of 'newdata' to the 'predict.mvgam' 
 #    function, returning conditional predictions on the response scale
 ?marginaleffects::plot_predictions
-plot_predictions(varcor_mod,
+plot_predictions(df_mod,
                  condition = c('?', '?', '?'),
                  conf_level = 0.8)
 
 
-# 2. Calculate the posterior median process error correlation matrix. 
-#    Which two algal groups are the most negatively correlated in their 
-#    process errors? Hint, see ?mvgam::as.matrix.mvgam and the 
-#    following template code for guidance (replace the ? with the correct value(s))
+# 2. Compare in-sample fits from the two models (`var_mod` and `df_mod`) 
+#    using `loo_compare()`. Does this comparison agree with the forecast 
+#    comparison above? Why might they differ?
+?mvgam::loo_compare.mvgam
 
-#    Each posterior draw of Sigma will be a vector of length 25, so you will 
-#    need to convert that vector to a matrix. Then you can see `?cov2cor` 
-#    for guidance on calculating correlation matrices from a covariance matrix
-#    You can use 'as.matrix.mvgam' to extract posterior estimates of 
-#    the process error matrix Sigma
-?mvgam::as.matrix.mvgam
-Sigmas <- as.matrix(varcor_mod, variable = '?', regex = '?')
 
-#    Each draw will be a vector of length 25 (representing the 
-#    5x5 covariance matrix); calculate posterior medians for each column
-?apply
-?median
-median_Sigma <- apply(Sigmas, '?', '?')
-
-# Convert to a correlation matrix
-?cov2cor
-cov2cor(matrix(median_Sigma, nrow = 5, ncol = 5))
+# 3. Fit a second dynamic factor model that uses Gaussian Process factors 
+#    in place of AR1 factors. Compare forecasts from this model to the AR1 
+#    factor model using the energy and variogram scores. Which model is 
+#    preferred?
